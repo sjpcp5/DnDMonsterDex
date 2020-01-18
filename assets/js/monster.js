2880307341d887f7bcf191978516f5b6b989cedc
tboyd4 <tboyd1898@gmail.com>
2020-01-18 13:21:27 -0600

    added click function to test click event handling on original list of results

--- assets/js/monster.js
+++ assets/js/monster.js
@@ -22,16 +22,17 @@ function callTheMonsters() {
         // these will be the if statements that will push our results into resultsToDisplay based upon parameters
         if (
           resultsSmall.size.includes("Medium") &&
-          resultsSmall.hit_points >= 100 &&
-          resultsSmall.hit_points <= 200 &&
-          resultsSmall.armor_class === 17
+          resultsSmall.hit_points >= 40 &&
+          resultsSmall.hit_points <= 60 &&
+          resultsSmall.armor_class > 11
         ) {
           resultsToDisplay.push(resultsSmall.name);
         }
         // This loop actually writes the results to the html file
         for (var j = 0; j < resultsToDisplay.length; j++) {
-          let newH1 = $("<h3>").text(resultsToDisplay[j]);
-          $(".display_data").append(newH1);
+          let newH3 = $("<h3>").text(resultsToDisplay[j]);
+          newH3.addClass("click_this");
+          $(".display_data").append(newH3);
         }
       });
     }
@@ -39,3 +40,18 @@ function callTheMonsters() {
 }
 
 callTheMonsters();
+
+// need a function that will display full stats upon clicking something
+
+$(".click_this").click(function() {
+  console.log("I am clicked");
+  let monster = this.text.toLowerCase();
+  console.log(monster);
+
+  $.ajax({
+    url: "http://www.dnd5eapi.co/api/monsters/" + monster,
+    method: "GET"
+  }).then(function(response3) {
+    console.log(response3);
+  });
+});
