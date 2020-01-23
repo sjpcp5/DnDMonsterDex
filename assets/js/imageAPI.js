befef9fcbc76bd08b84708a18872ac272b0ff92e
Cutie on the Couch <sjpcp5@gmail.com>
2020-01-23 12:26:15 -0600

    moved imageAPI.index to assests and js folder, created button and or event listener for image api

--- assets/js/imageAPI.js
+++ assets/js/imageAPI.js
@@ -15,18 +15,50 @@
  and chips, num "parameter is number of search items returned.""
 */
 //var DnDseResults = $(".display-data")
-
-var animal = $(this).attr("data-animal");
+$("button").on("click", function() {
+    
+var monster = $(this).attr("data-monster");
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animal + "&api_key=71957ReGgM9ed9MEpRgc0IVcliXGpSPq";
+        monster + "&api_key=71957ReGgM9ed9MEpRgc0IVcliXGpSPq";
 
-function imageResults(){
     $.ajax({
-        url: imgURL,
+        url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
 
-    })
-}
-imageResults();
+        var results = response.data;
+        // ========================
+
+         for (var i = 0; i < results.length; i++) {
+
+        // Step 3: uncomment the for loop above and the closing curly bracket below.
+        // Make a div with jQuery and store it in a variable named animalDiv.
+        // Make a paragraph tag with jQuery and store it in a variable named p.
+        // Set the inner text of the paragraph to the rating of the image in results[i].
+        // Make an image tag with jQuery and store it in a variable named animalImage.
+        // Set the image's src to results[i]'s fixed_height.url.
+        // Append the p variable to the animalDiv variable.
+        // Append the animalImage variable to the animalDiv variable.
+        // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
+
+        // ============= put step 3 in between these dashes ======================
+          var monsterDiv = $("<div>");
+          var rating = results[i].rating;
+          var p = $("<p>").text("Rating: " + rating);
+
+          var monsterImage = $("<img>");
+          monsterImage.attr("src", results[i].images.fixed_height.url);
+
+          monsterDiv.append(p);
+          monsterDiv.append(animalImage);
+
+          $("#gifs-appear-here").prepend(monsterDiv);
+
+          
+        // ==================================
+         }
+
+      });
+    });
+
