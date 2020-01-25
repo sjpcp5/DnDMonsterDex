8f248d7827a365705e6542b54b2790542e2b9bdb
tboyd4 <tboyd1898@gmail.com>
2020-01-25 10:52:28 -0600

    loading animation now works properly

--- assets/js/monster.js
+++ assets/js/monster.js
@@ -45,14 +45,14 @@ function callTheMonsters(
             let newH3 = $("<h3>").text(resultsToDisplay[j]); //if no name is chosen, it will return all monsters that fit args
             newH3.attr("data-name", resultsSmall.index);
             newH3.addClass("click_this");
-            $(".spinner-border").attr("style", "display: none;");
+            removeLoad();
             $(".display_data").append(newH3);
           } else {
             if (resultsToDisplay[j].includes(monName)) {
               let newH3 = $("<h3>").text(resultsToDisplay[j]); //if a name is chose, it will only display mons that include name
               newH3.attr("data-name", resultsSmall.index);
               newH3.addClass("click_this");
-              $(".spinner-border").attr("style", "display: none;");
+              removeLoad();
               $(".display_data").append(newH3);
             }
           }
@@ -62,12 +62,26 @@ function callTheMonsters(
   });
 }
 
+// function to add loading functionality
+
+function addLoad() {
+  let loadDiv = $("<div>");
+  loadDiv.addClass("spinner-border");
+
+  $(".display_data").append(loadDiv);
+}
+
+function removeLoad() {
+  $(".spinner-border").removeClass("spinner-border");
+}
+
 // this click listener will run the function above when they click search
 
 $("#display-results").on("click", function() {
   console.log("i hath been clicked");
   $("#monstPage").addClass("d-none");
-  $(".spinner-border").attr("style", "display: block;");
+
+  addLoad();
 
   let inputName = $("#name").val();
   let inputSize = "Large"; //$("#SizeSelect").html();
