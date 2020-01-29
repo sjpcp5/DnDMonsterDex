d273dcbe5cd860108b9cccc058c33f0f0b4f8977
Cutie on the Couch <sjpcp5@gmail.com>
2020-01-28 20:20:19 -0600

    updated style for results, results now have a red hover and underline action

--- assets/js/imageAPI.js
+++ assets/js/imageAPI.js
@@ -24,7 +24,7 @@
 
 // return randOffset;
 // };
-$("#giphy").remove();
+
 var counter = 0
 $(document).on("click", ".click_this", function() {
 console.log("monster has been clicked prepare image");
@@ -56,8 +56,9 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + monster + "&api_key=7
         url: queryURL,
         method: "GET"
     }).then(function(response) {
+      
         var results = response.data;
-        console.log(results);
+        
         // ========================
         
          for (var i = 0; i < results.length; i++) {
@@ -81,7 +82,7 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + monster + "&api_key=7
 
           //monsterDiv.append(p);
          // monsterDiv.append(monsterImage);
-          
+          $(".giphy").empty();
           $(".giphy").append(monsterImage);
           counter++;
           console.log("clicked.."+ counter);   
@@ -91,6 +92,7 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + monster + "&api_key=7
          });
          
 });
+
 
 // function gets the last word of the clicked monster
 function getLastWord(){
@@ -113,7 +115,11 @@ function GetOffset() {
 
 
 
-    
+//if statement if results do not match name of image do not append
+// need a function that nullifies append of image or do nothing
+// condition variable 
+// compare function of JSON data and monster variable 
+//if monster does not match results don't append img else append img
   
 
 
